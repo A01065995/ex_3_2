562b0ffaa64a53751a73a901371fb78328eff147
Gustavo <A01065995@itesm.mx>
2021-03-23 20:30:15 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ex3/childActivity.java
@@ -0,0 +1,18 @@
+package com.example.ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class childActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+
+    }
+}
